e0c7aaf48715e78203f65675eb04eac9c970d501
hyemin <hyemin_park@tmax.co.kr>
2023-08-17 13:27:35 +0900

    unit test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import './App.css';
 
 function App() {
   console.log('github integration build test');
+  console.log('unit test');
+
   return (
     <div className="App">
       <header className="App-header">
